e173bd48efb0baa22393191d700e982b5e9c8ef1
Isaac <isaacdelalama1013@gmail.com>
2022-03-27 19:33:17 -0500

    added board class to main

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,6 @@
 #include "Heuristic.h"
 #include "Tile.h"
+#include "Board.h"
 #include <string>
 #include <iostream>
 #include <vector>
@@ -8,84 +9,107 @@ std::vector<int> OPEN;
 
 std::vector<int> CLOSED;
 
+Board board1;
+Board board2;
 
 
 void createBoard1()
 {
+    
     Tile t1;
     t1.setPosition(8);
     t1.setGoalPosition(1);
+    board1.setTile(t1, 0);
 
     Tile t2;
     t2.setPosition(1);
     t2.setGoalPosition(2);
+    board1.setTile(t2, 1);
 
     Tile t3;
     t3.setPosition(3);
     t3.setGoalPosition(3);
+    board1.setTile(t3, 2);
 
     Tile t4;
     t4.setPosition(4);
     t4.setGoalPosition(4);
+    board1.setTile(t4, 3);
 
     Tile t5;
     t5.setPosition(5);
     t5.setGoalPosition(5);
+    board1.setTile(t5, 4);
 
     Tile t6;
     t6.setPosition(9);
     t6.setGoalPosition(6);
+    board1.setTile(t6, 5);
 
     Tile t7;
     t7.setPosition(6);
     t7.setGoalPosition(7);
+    board1.setTile(t7, 6);
     
     Tile t8;
     t8.setPosition(2);
     t8.setGoalPosition(8);
+    board1.setTile(t8, 7);
 
     Tile t9;
     t9.setPosition(8);
     t9.setGoalPosition(9);
+    board1.setTile(t9, 8);
+
 }
+
 
 void createBoard2()
 {
     Tile t1;
     t1.setPosition(8);
     t1.setGoalPosition(1);
+    board2.setTile(t1, 0);
 
     Tile t2;
-    t1.setPosition(1);
-    t1.setGoalPosition(2);
+    t2.setPosition(1);
+    t2.setGoalPosition(2);
+    board2.setTile(t2, 1);
 
     Tile t3;
-    t1.setPosition(3);
-    t1.setGoalPosition(3);
+    t3.setPosition(3);
+    t3.setGoalPosition(3);
+    board2.setTile(t3, 2);
 
     Tile t4;
-    t1.setPosition(4);
-    t1.setGoalPosition(4);
+    t4.setPosition(4);
+    t4.setGoalPosition(4);
+    board2.setTile(t4, 3);
 
     Tile t5;
-    t1.setPosition(5);
-    t1.setGoalPosition(5);
+    t5.setPosition(5);
+    t5.setGoalPosition(5);
+    board2.setTile(t5, 4);
 
     Tile t6;
-    t1.setPosition(9);
-    t1.setGoalPosition(6);
+    t6.setPosition(9);
+    t6.setGoalPosition(6);
+    board2.setTile(t6, 5);
 
     Tile t7;
-    t1.setPosition(6);
-    t1.setGoalPosition(7);
+    t7.setPosition(6);
+    t7.setGoalPosition(7);
+    board2.setTile(t7, 6);
     
     Tile t8;
-    t1.setPosition(2);
-    t1.setGoalPosition(8);
+    t8.setPosition(2);
+    t8.setGoalPosition(8);
+    board2.setTile(t8, 7);
 
     Tile t9;
-    t1.setPosition(8);
-    t1.setGoalPosition(9);
+    t9.setPosition(8);
+    t9.setGoalPosition(9);
+    board2.setTile(t9, 8);
 }
 
 void AStarAlgorithm()
