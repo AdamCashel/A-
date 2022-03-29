265b3acfca207ae8a342a4d66abc072e264b99f6
Adam Cashel <67659373+AdamCashel@users.noreply.github.com>
2022-03-29 17:00:12 -0500

    Help functions

--- Board.cpp
+++ Board.cpp
@@ -35,6 +35,11 @@ void Board::setFn(int FnVal){
     Fn_Value = FnVal;
 }
 
+bool operator==(const Board &temp1, const Board &temp2)
+{
+    return(temp1.getBoardArray() == temp2.getBoard());
+}
+
 int Board::getFn(){
     return Fn_Value;
 }
@@ -42,3 +47,5 @@ int Board::getFn(){
 Tile* Board::getBoardArray(){
     return board;
 }
+
+
